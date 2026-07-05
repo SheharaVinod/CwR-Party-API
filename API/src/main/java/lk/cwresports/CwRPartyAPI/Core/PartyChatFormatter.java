f83efaa2ff1156935b334f37bd37e2d98b9ba957
shehara winod <shehara.winod123@gmail.com>
2026-07-05 16:33:55 +0530

    feat: add PartyChatFormatter with configurable format and color stripping

---
+++ API/src/main/java/lk/cwresports/CwRPartyAPI/Core/PartyChatFormatter.java
@@ -0,0 +1,31 @@
+package lk.cwresports.CwRPartyAPI.Core;
+
+import org.bukkit.ChatColor;
+
+public class PartyChatFormatter {
+    public static final String DEFAULT_FORMAT = "&a%player_name% &f&l-&r &e{message}";
+
+    private String format;
+
+    public PartyChatFormatter(String format) {
+        this.format = format;
+    }
+
+    public PartyChatFormatter() {
+        this(DEFAULT_FORMAT);
+    }
+
+    public String format(String playerName, String message) {
+        String stripped = ChatColor.stripColor(ChatColor.translateAlternateColorCodes('&', message));
+        return ChatColor.translateAlternateColorCodes('&',
+                format.replace("%player_name%", playerName).replace("{message}", stripped));
+    }
+
+    public void setFormat(String format) {
+        this.format = format;
+    }
+
+    public String getFormat() {
+        return format;
+    }
+}
