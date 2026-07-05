f872bc558ca654b5e29a1e9920ad4b85fc1e6f15
shehara winod <shehara.winod123@gmail.com>
2026-07-05 16:36:12 +0530

    refactor: PartyChatCommand supports direct message, uses per-party PartyChat
    - /party-chat <msg> sends directly to party chat (no toggle)
    - /party-chat (no args) toggles party chat mode
    - PartyChatEventListener uses per-party PartyChat.sendMessage()
    - Removed static toggledPlayer set, uses per-party tracking

--- Plugin/src/main/java/lk/cwresports/CwRPartyAPI/Commands/PartyChatCommand.java
+++ Plugin/src/main/java/lk/cwresports/CwRPartyAPI/Commands/PartyChatCommand.java
@@ -1,5 +1,7 @@
 package lk.cwresports.CwRPartyAPI.Commands;
 
+import lk.cwresports.CwRPartyAPI.Core.Party;
+import lk.cwresports.CwRPartyAPI.Core.PartyChat;
 import lk.cwresports.CwRPartyAPI.Core.PartyManager;
 import lk.cwresports.CwRPartyAPI.Utils.TextStrings;
 import org.bukkit.command.Command;
@@ -7,12 +9,8 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class PartyChatCommand implements CommandExecutor {
     private static final String name = "party-chat";
-    private static final Set<Player> toggledPlayer = new HashSet<>();
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
@@ -22,6 +20,15 @@ public class PartyChatCommand implements CommandExecutor {
         Player player = (Player) commandSender;
 
         if (!PartyManager.getInstance().isInAParty(player)) {
+            player.sendMessage(TextStrings.colorize(TextStrings.YOU_ARE_NOT_IN_A_PARTY));
+            return true;
+        }
+
+        Party party = PartyManager.getInstance().getPartyOf(player);
+
+        if (strings.length > 0) {
+            String message = String.join(" ", strings);
+            party.getPartyChat().sendMessage(player, message);
             return true;
         }
 
@@ -30,27 +37,20 @@ public class PartyChatCommand implements CommandExecutor {
     }
 
     public static void togglePartyChat(Player player) {
-        if (isInPartyChat(player)) {
-            leavePartyChat(player);
+        if (!PartyManager.getInstance().isInAParty(player)) return;
+        PartyChat partyChat = PartyManager.getInstance().getPartyOf(player).getPartyChat();
+        partyChat.toggle(player);
+        if (partyChat.isToggled(player)) {
+            player.sendMessage(TextStrings.colorize("&7You are now in party chat."));
         } else {
-            joinPartyChat(player);
+            player.sendMessage(TextStrings.colorize("&7You are no longer in party chat."));
         }
     }
 
-    public static void joinPartyChat(Player player) {
-        toggledPlayer.add(player);
-        player.sendMessage(TextStrings.colorize("&7You are now in party chat."));
-    }
-
-    public static void leavePartyChat(Player player) {
-        toggledPlayer.remove(player);
-        player.sendMessage(TextStrings.colorize("&7You are no longer in party chat."));
-    }
-
     public static boolean isInPartyChat(Player player) {
-        return toggledPlayer.contains(player);
+        if (!PartyManager.getInstance().isInAParty(player)) return false;
+        return PartyManager.getInstance().getPartyOf(player).getPartyChat().isToggled(player);
     }
-
 
     public static String getName() {
         return name;
--- Plugin/src/main/java/lk/cwresports/CwRPartyAPI/Listeners/PartyChatEventListener.java
+++ Plugin/src/main/java/lk/cwresports/CwRPartyAPI/Listeners/PartyChatEventListener.java
@@ -4,7 +4,6 @@ import lk.cwresports.CwRPartyAPI.APIs.Events.PlayerRelated.PlayerLeavePartyEvent
 import lk.cwresports.CwRPartyAPI.Commands.PartyChatCommand;
 import lk.cwresports.CwRPartyAPI.Core.Party;
 import lk.cwresports.CwRPartyAPI.Core.PartyManager;
-import lk.cwresports.CwRPartyAPI.Utils.TextStrings;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -18,16 +17,17 @@ public class PartyChatEventListener implements Listener {
             if (PartyChatCommand.isInPartyChat(player)) {
                 event.setCancelled(true);
                 Party party = PartyManager.getInstance().getPartyOf(player);
-                for (Player member : party.getCopyOfAllMembersIncludingTheOwner()) {
-                    String msg = "&b" + player.getName() + " &f>>>&b " + event.getMessage();
-                    member.sendMessage(TextStrings.colorize(msg, false));
-                }
+                party.getPartyChat().sendMessage(player, event.getMessage());
             }
         }
     }
 
     @EventHandler
     public void onPlayerLeftFromParty(PlayerLeavePartyEvent event) {
-        PartyChatCommand.leavePartyChat(event.getLeavedPlayer());
+        Player player = event.getLeavedPlayer();
+        if (PartyManager.getInstance().isInAParty(player)) {
+            Party party = PartyManager.getInstance().getPartyOf(player);
+            party.getPartyChat().setToggled(player, false);
+        }
     }
 }
